19a29cad21f75b0b0ea242d8ae383afbe58489a5
Pietro Giordano <giopi87@gmail.com>
2023-01-11 17:28:41 +0100

    avanzamento bonus

---
+++ bonus/js/scripts.js
@@ -0,0 +1,51 @@
+/*
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+*/
+const gridGame = document.getElementById('grid-game');
+const play = document.getElementById('play');
+
+play.addEventListener('click',
+
+      function() {
+
+            gridGame.innerHTML = '';
+
+            const level = document.getElementById('level').value;
+
+            for(let i = 1; i <= level; i++) {
+
+                  const newCell = document.createElement('div');
+                  newCell.classList.add('cell');
+                  newCell.innerHTML = i;
+                  newCell.addEventListener('click',
+                  
+                        function() {
+
+                              if (this.classList.contains('clicked')) {
+                                    this.classList.remove('clicked');
+                              }
+                              else {
+                                    this.classList.add('clicked');
+                              }
+
+                              console.log('Hai cliccato la cella numero ' + i);
+
+                        }
+
+                  )
+
+                  gridGame.append(newCell);
+
+            }
+
+      }
+
+)
